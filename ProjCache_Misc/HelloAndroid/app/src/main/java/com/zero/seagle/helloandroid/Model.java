c3ed4aa7726221da12585c9226ef9bed1004cc5d
肖际 <seagle@Seagle-MacBook-Pro.local>
2019-06-06 11:17:30 -0500

    CardView implementation 2

---
+++ ProjCache_Misc/HelloAndroid/app/src/main/java/com/zero/seagle/helloandroid/Model.java
@@ -0,0 +1,21 @@
+package com.zero.seagle.helloandroid;
+
+public class Model {
+
+    private int image;
+
+
+    public Model(int image) {
+        this.image = image;
+
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
