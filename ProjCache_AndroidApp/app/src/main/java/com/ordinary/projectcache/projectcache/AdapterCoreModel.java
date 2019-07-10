939e0c3ae16c33569a12632aeadbd887a9275375
肖际 <seagle@Seagle-MacBook-Pro.local>
2019-07-10 16:26:10 -0500

    workable core

--- ProjCache_AndroidApp/app/src/main/java/com/ordinary/projectcache/projectcache/AdapterCoreModel.java
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/projectcache/projectcache/AdapterCoreModel.java
@@ -1,14 +1,18 @@
 package com.ordinary.projectcache.projectcache;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,7 +22,7 @@ public class AdapterCoreModel extends PagerAdapter {
     private LayoutInflater layoutInflater;
     private Context context;
 
-    public AdapterCoreModel(List<CoreModel> coreModels, Context context) {
+    public AdapterCoreModel(Context context, List<CoreModel> coreModels) {
         this.coreModels = coreModels;
         this.context = context;
     }
@@ -40,14 +44,26 @@ public class AdapterCoreModel extends PagerAdapter {
         View view = layoutInflater.inflate(R.layout.core_item, container, false);
 
         // set image for the core image button
-        ImageView imageViewCore;
-        imageViewCore = view.findViewById(R.id.core_imageView);
-        imageViewCore.setForeground(coreModels.get(position).getDrawable());
+        final ImageView coreImageView;
+        coreImageView = view.findViewById(R.id.core_imageView);
+        coreImageView.setImageDrawable(coreModels.get(position).getDrawable());
 
         // set text info for the current core card
-        TextView textViewCore;
-        textViewCore = view.findViewById(R.id.core_textView);
-        textViewCore.setText(coreModels.get(position).getText());
+        final TextView coreTextView;
+        coreTextView = view.findViewById(R.id.core_textView);
+        coreTextView.setText(coreModels.get(position).getText());
+
+        // set cardView for the core card
+        CardView coreCardView;
+        coreCardView = view.findViewById(R.id.core_CardView);
+        final int pos = position;
+        coreCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                context.startActivity(coreModels.get(pos).getIntent());
+
+            }
+        });
 
         // add the view to container
         container.addView(view, 0);
