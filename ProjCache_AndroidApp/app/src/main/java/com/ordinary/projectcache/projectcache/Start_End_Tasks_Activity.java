f93541306cfcb25bed83069cf9c9fe5fa04cd80c
440g22 <yli265@uic.edu>
2019-07-26 16:26:30 -0500

    Added new files

---
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/projectcache/projectcache/Start_End_Tasks_Activity.java
@@ -0,0 +1,48 @@
+package com.ordinary.projectcache.projectcache;
+
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Start_End_Tasks_Activity extends AppCompatActivity {
+
+    private final int REQUEST_APP_LIST_CODE = 1001;
+    Boolean app = false;
+    Boolean QR = false;
+    Button openApp;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_end_tasks_activity);
+        viewPager = (ViewPager) findViewById(R.id.setup_viewPager);
+        openApp = (Button) findViewById(R.id.open_app);
+        openApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                app = true;
+                Intent intent = new Intent(Start_End_Tasks_Activity.this, Applist_Activity.class);
+                startActivityForResult(intent, REQUEST_APP_LIST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (app) {
+            try {
+                data.putExtra("Application", "");
+            } catch (NullPointerException e) {
+            }
+        } else if (QR) {
+            data.putExtra("QR", "");
+        }
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+}
