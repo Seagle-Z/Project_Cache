4c2167c86ce47a096b1b4f2366d77db1d153bd81
DrNoodlesthederp <bdevil2@uic.edu>
2019-08-24 03:28:29 -0500

    EventSetupPage1Fragment.java added the WIFI functionality when 'editing'

--- ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/SetupEventConditionWifiActivity.java
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/SetupEventConditionWifiActivity.java
@@ -1,15 +1,10 @@
 package com.ordinary.android.projectcache;
 
 import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -20,49 +15,39 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.app.Activity;
-
-import java.net.NetworkInterface;
+//import android.content.BroadcastReceiver;
+//import android.content.IntentFilter;
+//import android.content.pm.PackageManager;
+//import android.net.wifi.WifiConfiguration;
+//import android.net.wifi.WifiInfo;
+//import java.util.Collections;
+//import java.net.NetworkInterface;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class SetupEventConditionWifiActivity extends AppCompatActivity {
 
     //Code for WIFISelectorActivity result request only
     private final int WIFI_PICKING_CODE = 1014;
-    ArrayList<String> StoredWifi = new ArrayList<>();
+    //ArrayList<String> StoredWifi = new ArrayList<>();
+
     //User-Interface Code
     Button addWifiButton, completeButton;
     ListView selectedWIFIListView;
     Context wifi_picker_context;
     ArrayAdapter wifiLISTAdapterView;
     //Local Variables
-    private WifiManager wifiManager;
-    private int size = 0;
-    private List<ScanResult> results;
     private ToolFunctions TF = new ToolFunctions();
     private WIFIInfoModel returnedWifi;
-    private List<String> selectedWifiArrList = new ArrayList<String>(); // "YOU CHOOSE: UIC WIFI"
-    private List<String> selectedWifiSSIDArrList = new ArrayList<String>(); //UIC.WIFI
-    private List<String> encryptedSSIDArrList = new ArrayList<String>(); //91-24-123
+    private List<String> selectedWifiArrList = new ArrayList<String>();     //Ex) {UIC-WIFI, Guest, MyHome}
+    private List<String> encryptedSSIDArrList = new ArrayList<String>();    //Ex) {91-24-123#43-35-62}
     private boolean editMode;
     private int selectedEditPosition;
     private AlertDialog.Builder warning;
-
-//    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            results = wifiManager.getScanResults();
-//            unregisterReceiver(wifiReceiver);
-//
-//            for (ScanResult scanResult : results) {
-//                StoredWifi.add(scanResult.SSID);             //Saves into Scan Results
-//                System.out.println("SSID: " + scanResult.SSID + "       MAC: " + scanResult.BSSID);
-//                wifiLISTAdapterView.notifyDataSetChanged();
-//                TF.setListViewHeightBasedOnChildren(wifiLISTAdapterView, selectedWIFIListView);
-//            }
-//        }
-//    };
+    private String retrieveWIFIList;
+    private WifiManager wifiManager;
+    //private int size = 0;
+    //private List<ScanResult> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,9 +61,7 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
         selectedWIFIListView = findViewById(R.id.selected_wifi_list);
         warning = new AlertDialog.Builder(wifi_picker_context);
 
-
         selectedWIFIListView.setTextFilterEnabled(true);
-//        adapterForTimeListView = new ArrayAdapter<String>(app_picker_context, R.layout.layout_app_list, R.id.condition_name, selectedAppArrList);
 
         wifiLISTAdapterView = new ArrayAdapter<String>(
                 wifi_picker_context,
@@ -91,30 +74,10 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
 
         wifiManager = (WifiManager) wifi_picker_context.getSystemService(Context.WIFI_SERVICE);
 
-        //Scan the WIFI-device currently
-        //scanWifi();
-
         addWifiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 editMode = false;
-
-                /*
-                //CURRENT WIFI (MOVE THIS)
-                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-                System.out.println("CURRENTLY CONNECTED: " + wifiInfo.getSSID() + "     MAC: " + getMacAddr());
-
-                //PREVIOUSLY SAVED (MOVE THIS)
-                List<WifiConfiguration> currentNetworks = wifiManager.getConfiguredNetworks();
-                System.out.println("Previously Saved WIFI");
-                for (WifiConfiguration test : currentNetworks) {
-                    System.out.println("SSID: " + test.SSID);
-                }
-
-                System.out.println("Selecting WIFI");
-                //Scan the WIFI-device currently
-                scanWifi();
-                */
 
                 Intent intent = new Intent(wifi_picker_context, WIFISelectorActivity.class);
                 startActivityForResult(intent, WIFI_PICKING_CODE);
@@ -125,23 +88,88 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
         completeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: 8/22/2019  check if all result are valid
-                // TODO: 8/22/2019 encrypt data (implement in ToolFunction)
-                // TODO: 8/22/2019 dump that in the list and return it
-                // Encoded Code
-                //  int temp [] = TF.asciiEncoder(returnedWifi.getWIFIName());
-                //  for(int i : temp)
-                //  {
-                //      System.out.println(i);
-                // }
+                try {
+                    if (!selectedWifiArrList.isEmpty()) {
+                        String intentResult = "";
 
-                // Decoded Code
-                //  int [] tmep = {86,74,68};
-                //  System.out.println(TF.asciiDecoder(tmep));
+                        for (int i = 0; i < selectedWifiArrList.size(); i++) {                               //Unencoded List   Ex) selectedAppArrList = {UIC, Guest, DeVilla, ..}
+
+                            // Encoding the String
+                            int tempEncoded [] = TF.asciiEncoder(selectedWifiArrList.get(i));
+                            String tempString = "";
+
+                            //Padding the string with '-' between each number
+                            for(int j = 0; j < tempEncoded.length; j++)
+                            {
+                                if (j != tempEncoded.length - 1)
+                                    tempString = tempString + tempEncoded[j] + "-";
+                                else
+                                    tempString = tempString + tempEncoded[j];
+                            }
+                            encryptedSSIDArrList.add(tempString);
+                        }
+
+                        //Append everything in the encryptedSSIDArrList into intentResult string;
+                        for (int k = 0; k < encryptedSSIDArrList.size(); k++) {
+                            if (k != encryptedSSIDArrList.size() - 1)
+                                intentResult = intentResult + encryptedSSIDArrList.get(k) + "#";
+                            else
+                                intentResult = intentResult + encryptedSSIDArrList.get(k);
+                        }
+
+                        Intent intent = new Intent();
+                        intent.putExtra("Wifi", intentResult);
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    } else {
+                        AlertDialog.Builder adb = new AlertDialog.Builder(wifi_picker_context);
+                        adb.setTitle("Warning");
+                        adb.setMessage(
+                                "Please add at least one WIFI for the event"
+                        );
+                        adb.setPositiveButton("Ok",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        adb.show();
+                    }
+                } catch (NullPointerException e) {
+                }
 
             }
         });
-    }
+
+        Intent intent = getIntent();
+        try {
+            if (intent.hasExtra("RETRIEVE")) {
+                retrieveWIFIList = intent.getStringExtra("RETRIEVE");                   //Encoded String must be decoded.
+                String[] wifiStringDivider = retrieveWIFIList.split("#");               //Splits each string up from '#' delim.
+
+                for (int i = 0; i < wifiStringDivider.length; i++) {
+                    String selectedEncodedString = wifiStringDivider[i];
+                    String[] tempNumArray = selectedEncodedString.split("-"); // splitting string by '-'
+
+                    int tempNumSize = tempNumArray.length;
+                    int tempNum[] = new int[tempNumSize];
+
+                    //Converting String Integer Array into Integer Integer Array
+                    for (int z= 0; z < tempNumArray.length; z++) {
+                        tempNum[z] = Integer.parseInt(tempNumArray[z]);
+                    }
+
+                    selectedWifiArrList.add(TF.asciiDecoder(tempNum)); //Add Decoded String onto the wifiList
+                }
+
+                wifiLISTAdapterView.notifyDataSetChanged();
+                TF.setListViewHeightBasedOnChildren(wifiLISTAdapterView, selectedWIFIListView);
+            }
+        } catch (NullPointerException e) {
+        }
+
+    } //End of onCreate()
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -153,7 +181,7 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
                     if (returnedWifi != null) {
                         if (!checkDuplicate(returnedWifi.getWIFIName(), selectedWifiArrList)) {
                             if (!editMode) {
-                                selectedWifiArrList.add(returnedWifi.getWIFIName());
+                                selectedWifiArrList.add(returnedWifi.getWIFIName());                //Places the data from the WifiSelectorActivity.java into the arraylist
                             } else {
                                 selectedWifiArrList.set(
                                         selectedEditPosition,
@@ -176,42 +204,6 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
         }
     }
 
-
-//    private void scanWifi() {
-//        StoredWifi.clear();
-//        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-//        wifiManager.startScan();
-//        Toast.makeText(wifi_picker_context, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
-//    }
-
-
-    public String getMacAddr() {
-        try {
-            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface nif : all) {
-                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-
-                byte[] macBytes = nif.getHardwareAddress();
-                if (macBytes == null) {
-                    return "";
-                }
-
-                StringBuilder res1 = new StringBuilder();
-                for (byte b : macBytes) {
-                    //res1.append(Integer.toHexString(b & 0xFF) + ":");
-                    res1.append(String.format("%02X:", b));
-                }
-
-                if (res1.length() > 0) {
-                    res1.deleteCharAt(res1.length() - 1);
-                }
-                return res1.toString();
-            }
-        } catch (Exception ex) {
-        }
-        return "";
-    }
-
     public boolean checkDuplicate(String wifi, List<String> wifilist) {
         if (!wifilist.isEmpty()) {
             for (String wifiInfoModel : wifilist) {
@@ -230,4 +222,54 @@ public class SetupEventConditionWifiActivity extends AppCompatActivity {
         }
         return false;
     }
+
+//    private void scanWifi() {
+//        StoredWifi.clear();
+//        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+//        wifiManager.startScan();
+//        Toast.makeText(wifi_picker_context, "Scanning WiFi ...", Toast.LENGTH_SHORT).show();
+//    }
+
+//    public String getMacAddr() {
+//        try {
+//            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+//            for (NetworkInterface nif : all) {
+//                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+//
+//                byte[] macBytes = nif.getHardwareAddress();
+//                if (macBytes == null) {
+//                    return "";
+//                }
+//
+//                StringBuilder res1 = new StringBuilder();
+//                for (byte b : macBytes) {
+//                    //res1.append(Integer.toHexString(b & 0xFF) + ":");
+//                    res1.append(String.format("%02X:", b));
+//                }
+//
+//                if (res1.length() > 0) {
+//                    res1.deleteCharAt(res1.length() - 1);
+//                }
+//                return res1.toString();
+//            }
+//        } catch (Exception ex) {
+//        }
+//        return "";
+//    }
+
+//    Used for Scanning WIFIs on device
+//    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+//        @Override
+//        public void onReceive(Context context, Intent intent) {
+//            results = wifiManager.getScanResults();
+//            unregisterReceiver(wifiReceiver);
+//
+//            for (ScanResult scanResult : results) {
+//                StoredWifi.add(scanResult.SSID);             //Saves into Scan Results
+//                System.out.println("SSID: " + scanResult.SSID + "       MAC: " + scanResult.BSSID);
+//                wifiLISTAdapterView.notifyDataSetChanged();
+//                TF.setListViewHeightBasedOnChildren(wifiLISTAdapterView, selectedWIFIListView);
+//            }
+//        }
+//    };
 }
