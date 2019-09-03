a07cd509cfc9997d19485e77670084d1c8b1095d
Yushen Li <Yushen.li1996@gmail.com>
2019-09-03 10:41:37 -0500

    event file close and alertdialog

--- ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/EventSetupPage1Fragment.java
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/EventSetupPage1Fragment.java
@@ -1,11 +1,12 @@
 package com.ordinary.android.projectcache;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,8 +14,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -116,12 +115,19 @@ public class EventSetupPage1Fragment
         forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!conditionsArrList.isEmpty())
-                {
+                if (!conditionsArrList.isEmpty()) {
                     viewPager.setCurrentItem(1);
-                }
-                else {
-                    Toast.makeText(getContext(), "Please add a condition first", Toast.LENGTH_SHORT).show();
+                } else {
+                    AlertDialog.Builder warning = new AlertDialog.Builder(getContext());
+                    warning.setTitle("Warning");
+                    warning.setMessage("Please selected at least one condition first.");
+                    warning.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    warning.show();
                 }
             }
         });
