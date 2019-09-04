8947b458b857e05cf3bfa440e2f2f06b29d5b9d5
Yushen Li <Yushen.li1996@gmail.com>
2019-09-03 21:55:46 -0500

    Shrink code for SetupEventConditionWifiActivity.java

--- ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/SetupEventConditionWifiActivity.java
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/SetupEventConditionWifiActivity.java
@@ -11,8 +11,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
