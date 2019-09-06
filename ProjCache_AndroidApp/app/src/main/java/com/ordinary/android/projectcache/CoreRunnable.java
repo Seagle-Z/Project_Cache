b11a01024707326d34ed08502d225e8e5f0fe2d3
Ji Xiao <seaglexiao@gmail.com>
2019-09-05 19:55:34 -0500

    sep 5 19:55

--- ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/CoreRunnable.java
+++ ProjCache_AndroidApp/app/src/main/java/com/ordinary/android/projectcache/CoreRunnable.java
@@ -13,23 +13,32 @@ import java.util.List;
 public class CoreRunnable implements Runnable {
 
     private Context context;                // MainActivity's context
+    private File eventsFile;
+
     private ViewPager coreViewPager;
     private Handler mainHandler;
+
     private Events events;
-    private CoreModel coreModelDefaultEvent;
+    private List<Event> eventsList;
     private List<Integer> activatedEventsID;
     private List<Integer> triggerableEventsID;
     private List<Integer> runningEventsID;
-    private File eventsFile;
+
+    private CoreModel coreModelDefaultEvent;
+
 
     private static final String EVENTS_FILE_NAME = "events.csv";
 
     CoreRunnable(Context context, File eventsFile, ViewPager coreViewPager) {
-        this.context = context;
+
         this.coreViewPager = coreViewPager;
         this.mainHandler = new Handler(context.getMainLooper());
+
+        this.context = context;
         this.eventsFile = eventsFile;
-        this.events = new Events(context, eventsFile);
+
+        this.events = new Events(context);
+
         this.runningEventsID = new ArrayList<>();
     }
 
@@ -39,26 +48,27 @@ public class CoreRunnable implements Runnable {
             @Override
             public void run() {
                 // Put the Default event into core
-                CoreTasksExecutor coreTasksExecutor =
-                        new CoreTasksExecutor(context, events.getDefaultEvent());
-
-                coreModelDefaultEvent = new CoreModel(
-                        coreTasksExecutor,
-                        events.getDefaultEvent().eventName,
-                        context.getResources().getDrawable(R.drawable.ic_menu_send, null)
-                );
-
-                List<CoreModel> coreModels = new ArrayList<>();
-                coreModels.add(coreModelDefaultEvent);
-
-                CoreModelAdapter coreModelAdapter = new CoreModelAdapter(context, coreModels);
-                coreViewPager.setAdapter(coreModelAdapter);
+//                CoreTasksExecutor coreTasksExecutor =
+//                        new CoreTasksExecutor(context, events.getDefaultEvent());
+//
+//                coreModelDefaultEvent = new CoreModel(
+//                        coreTasksExecutor,
+//                        events.getDefaultEvent().eventName,
+//                        context.getResources().getDrawable(R.drawable.ic_menu_send, null)
+//                );
+//
+//                List<CoreModel> coreModels = new ArrayList<>();
+//                coreModels.add(coreModelDefaultEvent);
+//
+//                CoreModelAdapter coreModelAdapter = new CoreModelAdapter(context, coreModels);
+//                coreViewPager.setAdapter(coreModelAdapter);
 
             }
         });
 
         triggerableEventsID = new ArrayList<>();
         runningEventsID = new ArrayList<>();
+        events = new Events(context);
         for (; ; ) {
 
             runOnUiThread(new Runnable() {
@@ -68,11 +78,12 @@ public class CoreRunnable implements Runnable {
                      * 让 YSL 在每次 event 新的event设置完成后在MainActivity里放一个static用来标记有events.csv有变化，
                      * 这样CoreRunnable就不需要一直读取csv文件了
                      */
-//                    if (MainActivity.eventsChanged == true) {
-//                        events = new Events(context, eventsFile);
-//                    }
-                    File ef = new File(context.getFilesDir(), EVENTS_FILE_NAME);
-                    events = new Events(context, ef);
+
+                    /* After b_ysl finish add new events and modify/delete, add checking here. only update eventsList when events change */
+                    if (true /* eventsListChanged */) {
+                        events.updateEventsList();
+                    }
+
                     activatedEventsID = events.getActivatedEventsIDList();
                     CoreConditionInspector cci = new CoreConditionInspector(context, events);
                     triggerableEventsID = cci.getTriggerableEventsID();
@@ -135,80 +146,14 @@ public class CoreRunnable implements Runnable {
         mainHandler.post(r);
     }
 
+    private List<Integer> updateActivatedEventsID() {
 
-    // Hard Code for development and testing
-//    @Override
-//    public void run() {
-//        for (int i = 0; i < 200; i++) {
-//            if (i % 2 == 0) {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        String url = "http://www.google.com";
-//                        Intent testIntent2 = new Intent(Intent.ACTION_VIEW);
-//                        testIntent2.setData(Uri.parse(url));
-//                        List<CoreModel> coreModels = new ArrayList<>();
-//                        coreModels.add(new CoreModel(
-//                                testIntent2,
-//                                context.getResources().getDrawable(
-//                                        R.drawable.ic_menu_gallery, null),
-//                                "abc"
-//                        ));
-//                        coreModels.add(new CoreModel(
-//                                testIntent2,
-//                                context.getResources().getDrawable(
-//                                        R.drawable.ic_menu_share, null),
-//                                "def"
-//                        ));
-//                        CoreModelAdapter coreModelAdapter =
-//                                new CoreModelAdapter(context, coreModels);
-//                        coreViewPager.setAdapter(coreModelAdapter);
-//                        System.out.println("en...????");
-//                    }
-//                });
-//
-//            } else {
-//                runOnUiThread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        String url = "http://www.bing.com";
-//                        Intent testIntent2 = new Intent(Intent.ACTION_VIEW);
-//                        testIntent2.setData(Uri.parse(url));
-//                        List<CoreModel> coreModels = new ArrayList<>();
-//                        coreModels.add(new CoreModel(
-//                                testIntent2,
-//                                context.getResources().getDrawable(
-//                                        R.drawable.ic_menu_slideshow, null),
-//                                "123"
-//                        ));
-//                        coreModels.add(new CoreModel(
-//                                testIntent2,
-//                                context.getResources().getDrawable(
-//                                        R.drawable.ic_menu_manage, null),
-//                                "456"
-//                        ));
-//                        coreModels.add(new CoreModel(
-//                                testIntent2,
-//                                context.getResources().getDrawable(
-//                                        R.drawable.ic_menu_gallery, null),
-//                                "789"
-//                        ));
-//                        CoreModelAdapter coreModelAdapter =
-//                                new CoreModelAdapter(context, coreModels);
-//                        coreViewPager.setAdapter(coreModelAdapter);
-//                        System.out.println("en...!!!!");
-//                    }
-//                });
-//            }
-//            try {
-//                Thread.sleep(2000);
-//            } catch (Exception e) {
-//            }
-//            System.out.println("en...xxxx");
-//        }
-//    }
 
-    private String printThis(String[] sArr) {
+        return null;
+    }
+
+
+    private String printStringArray(String[] sArr) {
         if (sArr == null) {
             return "NULL";
         }
